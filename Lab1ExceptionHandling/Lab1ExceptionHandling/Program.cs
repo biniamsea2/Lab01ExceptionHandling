98ece27d3f81bd6dadfca2e2cb241f36766c03c8
Biniam <biniamt23@yahoo.com>
2019-10-15 10:33:38 -0700

    Test 1

---
+++ Lab1ExceptionHandling/Lab1ExceptionHandling/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab1ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartSequence();
+        }
+
+        private static void StartSequence()
+        {
+            Console.WriteLine("Enter a number greater than zero");
+            int result = int.Parse(Console.ReadLine());
+        }
+
+    }
+}
